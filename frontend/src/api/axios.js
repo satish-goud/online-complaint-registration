bf86c8b18aa8b498d351f9cfcb5284e5b7f15d62
satish-goud <tanguturisatish16@gmail.com>
2026-06-18 19:34:54 +0530

    fix backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://online-complaint-backend.onrender.com/api",
+  baseURL: "https://online-complaint-backend-y2ih.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
